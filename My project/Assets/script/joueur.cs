74f56aa181c1c81adc0b59dc12730ab4712099d1
AlexisBolduc1 <93773484+AlexisBolduc1@users.noreply.github.com>
2023-11-28 10:41:00 -0500

    test jump

--- My project/Assets/script/joueur.cs
+++ My project/Assets/script/joueur.cs
@@ -6,23 +6,33 @@ using UnityEngine.Video;
 public class joueur : MonoBehaviour
 {
     public int count;
+    public GameObject skydefault;
+    public GameObject skyville;
 
     
 private void OnTriggerEnter(Collider other){
+        if (other.tag == "ville")
+        {
+            skyville.SetActive(true);
+            skydefault.SetActive(false);
+        }
 
-
-}
+    }
 
 private void OnTriggerExit(Collider other){
     if(other.tag == "livre")
     {
         other.gameObject.SetActive(false);
         count ++;
+    }else if (other.tag == "ville")
+        {
+            skyville.SetActive(false);
+            skydefault.SetActive(true);
+        }
+
+
+
     }
-
-
-
-}
 
 
 
